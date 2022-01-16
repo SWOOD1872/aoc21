700d4efb2bc52e4f48dccf32799dfb828d712c6d
Sam Wood <samwooddev@gmail.com>
2022-01-16 13:10:23 +0000

    Day 6 part 2 added to the CLI

--- cmd/day6.go
+++ cmd/day6.go
@@ -97,6 +97,61 @@ var day6Part2Cmd = &cobra.Command{
 
 // day6Part2 is the part 2 solution code
 func day6Part2(cmd *cobra.Command, args []string) error {
+	data, err := os.Open(input)
+	if err != nil {
+		return err
+	}
+	defer data.Close()
+
+	scanner := bufio.NewScanner(data)
+	var nums []int
+	var num int
+	for scanner.Scan() {
+		line := scanner.Text()
+		numsStr := strings.Split(line, ",")
+		for _, numStr := range numsStr {
+			num, err = strconv.Atoi(numStr)
+			if err != nil {
+				panic(err)
+			}
+			nums = append(nums, num)
+		}
+	}
+
+	var bucket = map[int]int{
+		0: 0,
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
+		5: 0,
+		6: 0,
+		7: 0,
+		8: 0,
+	}
+
+	for _, n := range nums {
+		bucket[n] += 1
+	}
+
+	days := 256
+	cycle := 7
+	newCycle := 9
+	for d := 0; d < days; d++ {
+		birthingFish := bucket[0]
+		for i := 0; i < newCycle-1; i++ {
+			bucket[i] = bucket[i+1]
+		}
+		bucket[cycle-1] += birthingFish
+		bucket[newCycle-1] = birthingFish
+	}
+
+	var sums int
+	for _, n := range bucket {
+		sums += n
+	}
+	fmt.Printf("Answer: %v\n", sums)
+
 	return nil
 }
 
