e7d4db18a8fea79e1e0e8e0c9a3eb5ab615a84f8
sam wood <samwood668@gmail.com>
2022-01-16 01:59:16 +0000

    Day 3 part 2 added to the CLI

--- cmd/day3.go
+++ cmd/day3.go
@@ -81,6 +81,24 @@ var day3Part2Cmd = &cobra.Command{
 
 // day3Part2 is the part 2 solution code
 func day3Part2(cmd *cobra.Command, args []string) error {
+	data, err := os.Open(input)
+	if err != nil {
+		return err
+	}
+	defer data.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	oxygenRating := binToDec(rating(lines, "most"))
+	co2Rating := binToDec(rating(lines, "least"))
+	lifeSupportRating := oxygenRating * co2Rating
+	fmt.Printf("Answer: %v\n", lifeSupportRating)
+
 	return nil
 }
 
@@ -88,6 +106,52 @@ func day3Part2(cmd *cobra.Command, args []string) error {
 func binToDec(s string) int {
 	a, _ := strconv.ParseInt(s, 2, 64)
 	return int(a)
+}
+
+func rating(list []string, s string) string {
+	answer := ""
+	n := len(list[0])
+	z, o := 0, 0
+	for i := 0; i < n; i++ {
+		mcn := ""
+		for _, line := range list {
+			if string(line[i]) == "0" {
+				z += 1
+			} else {
+				o += 1
+			}
+		}
+		if s == "most" {
+			if z > o {
+				mcn = "0"
+			} else {
+				mcn = "1"
+			}
+		} else if s == "least" {
+			if z > o {
+				mcn = "1"
+			} else {
+				mcn = "0"
+			}
+		}
+		z = 0
+		o = 0
+		list = filter(list, i, mcn)
+		if len(list) == 1 {
+			answer = list[0]
+		}
+	}
+	return answer
+}
+
+func filter(list []string, pos int, filter string) []string {
+	var filtered []string
+	for _, item := range list {
+		if string(item[pos]) == filter {
+			filtered = append(filtered, item)
+		}
+	}
+	return filtered
 }
 
 func init() {
